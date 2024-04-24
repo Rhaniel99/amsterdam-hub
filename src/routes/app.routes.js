8f9372660001c6add223ec56c4de3811884b34f2
rhaniel99 <rhanielmonteiro.13@gmail.com>
2024-04-24 01:06:52 -0400

    ajustes criando uma nova rota

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -8,6 +8,18 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.post("/data", async (req, res) => {
+    const { name, desc } = req.body;
+    try {
+      // Inserir os dados no banco de dados usando Sequelize
+      const newUser = await Tag.create({ name, desc }); // Cria um novo usuário com os dados fornecidos
+      res.json({ message: "User added successfully", userId: newUser.id });
+    } catch (error) {
+      console.error('Error adding user:', error);
+      res.status(500).json({ error: 'Error adding user' });
+    }
+  });
+
 // router.get('/quotes', async (req, res) => {
 //     try {
 //         const quotes = await Quote.findAll();
