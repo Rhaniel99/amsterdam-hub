1f62ce3649e82dfe2ea230a9343f99b57d5558d7
rhaniel99 <rhanielmonteiro.13@gmail.com>
2024-04-29 16:46:16 -0400

    novidades em breve

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@ import 'dotenv/config';
 import './src/configs/discord.config.js';
 
 import express from 'express';
+import cors from 'cors';
 const PORT = process.env.PORT || 6060;
 const app = express();
 import database from './src/configs/db.config.js'; // Caminho para o arquivo de rotas
 import rotas from './src/routes/app.routes.js'; // Caminho para o arquivo de rotas
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/api/app", rotas);
 
 app.listen(PORT, async () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import GlobalStyle from './styles/global';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import styled from "styled-components";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to rsrsrs.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <ToastContainer auytoClose={3000}  position="toast.POSITION.BOTTOM_LEFT" />
+  <GlobalStyle />
+  </>
   );
 }
 
