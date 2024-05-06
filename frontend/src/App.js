d49ad33853968df3e29682332b8d27d4a1ea30b1
rhaniel99 <rhanielmonteiro.13@gmail.com>
2024-05-06 16:51:39 -0400

    ajustes finais do dia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,44 @@
-import GlobalStyle from './styles/global';
-import { toast, ToastContainer } from "react-toastify";
+import React, { Component } from 'react';
 
-import Form from "./components/Form/Form.js";
-import Grid from "./components/Grid/Grid.js";
-import { useState, useEffect } from "react";
-import "react-toastify/dist/ReactToastify.css";
-import styled from "styled-components";
-import axios from "axios";
-
-const Container = styled.div`
-      width: 100%;
-      max-width: 800px;
-      margin-top: 20px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 10px;
-`;  
-
-const Title = styled.h2``;
-
-function App() {
-  const [tools, setTools] =  useState([]);
-  const [onEdit, setOnEdit] =  useState(null);
-
-  const getTools = async () => {
-    try{
-      const res = await axios.get("http://localhost:6060/api/app/get-tools");
-      setTools(res.data.sort((a, b) => (a.name > b.name ? 1 : -1)));
-    }catch(err){
-      toast.error(err);
-    }
-  };
-
-  useEffect(() => {
-    getTools();
-  }, [setTools]);
-
-  return (
-    <>
-    <Container>
-        <Title>Cadastrar Ferramentas</Title>
-        <Form getTools={getTools}/>
-        <Grid tools={tools}/>
-    </Container>
-    <ToastContainer autoClose={3000} position="bottom-left"  />
-    <GlobalStyle />
-    </>
-    );
+class Equipe extends Component{
+  render(){
+    return(
+      <div>
+        <Sobre {...this.props}/>
+        <hr/>
+      </div>
+    )
   }
+}
+
+class Sobre extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Ola sou o {this.props.nome} </h2>
+        <h3> Cargo:  {this.props.cargo} </h3>
+        <h3> Idade:  {this.props.idade} anos</h3>
+      </div>
+    )
+  }
+}
+
+// const Social = (props) => {
+//   return(
+//     <div>
+//       <a>Facebook</a>    
+//       <a>Linkedin</a>    
+//     </div>
+//   )
+// }
+ 
+function App() {
+  return (
+  <div>
+    <h1>Conheça nossa equipe: </h1>
+    <Equipe nome="Rhans" cargo="Desenvolvedor" idade="22"/>
+  </div>
+  )
+}
 
 export default App;
