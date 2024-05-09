b811cb68c088c1942c2fe85ebfc7457c298eee33
rhaniel99 <rhanielmonteiro.13@gmail.com>
2024-05-09 16:48:47 -0400

    biscoito da sorte project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro';
 
+class App extends Component{
+    constructor (props) {
+        super(props);
+        this.state = {
 
-class App extends Component {
+        }
+        this.frases = ['O riso é a menor distância entre duas pessoas.', 
+        'Deixe de lado as preocupações e seja feliz.',
+        'Realize o óbvio, pense no improvável e conquiste o impossível.',
+        'Acredite em milagres, mas não dependa deles.',
+        'A maior barreira para o sucesso é o medo do fracasso.'];
 
-  
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-        <Membro nome="Visitante"/>
-      </div>
-    );
-  }
+    }
+
+    render () {
+        return (
+            <div>
+              <img src={require('./assets/cinema.jpg')} />
+              <Botao />
+              <h3>Filme número 1 aleatorio</h3>
+            </div>
+        )
+    }
+}
+
+class Botao extends Component {
+
+    render () {
+        return (
+            <div>
+              <button>Abrir o filme</button>
+            </div>
+        )
+    }      
 }
 
 export default App;
